e6dc5f8c9a83c663942b64dffa8bc181ef0ccd1d
samueljude <50998233+samueljude@users.noreply.github.com>
2020-10-17 12:33:23 +0530

    Add files via upload

---
+++ build/precache-manifest.b8ab83ddb40b7b645e3d1c1b4a5e4d6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f7981073b341193bc2914c85e9057f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56377a890865ebe2b9f7",
+    "url": "/static/css/main.4757b309.chunk.css"
+  },
+  {
+    "revision": "71e1ae39800070b02832",
+    "url": "/static/js/2.173c6667.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.173c6667.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56377a890865ebe2b9f7",
+    "url": "/static/js/main.6504f992.chunk.js"
+  },
+  {
+    "revision": "72017dee5443dfc32ffd",
+    "url": "/static/js/runtime-main.dc49cf19.js"
+  }
+]);
